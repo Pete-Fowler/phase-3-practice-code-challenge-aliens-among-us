d10207e9a0bc943fedc4f3a72b113670cb34c7e7
Pete Fowler <fowler.pete@gmail.com>
2022-09-27 09:57:59 -0600

    Add commas, narration to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+puts "Seeding ..."
+
 10.times do
   Earthling.create(
     first_name: Faker::Name.first_name, 
@@ -6,15 +8,17 @@
   )
 
   Alien.create(
-    name: Faker::Name.first
-    earth_disguise_name: Faker::Name.first
-    home_planet: Faker::Space.planet
+    name: Faker::Name.first,
+    earth_disguise_name: Faker::Name.first,
+    home_planet: Faker::Space.planet,
     light_years_to_home_planet: Faker::Space.distance_measurement
   )
 
   Visitation.create(
-    date: Faker::Date.between(from: 2020-01-01 to: 2022-09-27)
-    alien_id: Faker::Number.unique.non_zero_digit
+    date: Faker::Date.between(from: 2020-01-01 to: 2022-09-27), 
+    alien_id: Faker::Number.unique.non_zero_digit, 
     earthling_id: Faker::Number.unique.non_zero_digit
   )
 end
+
+puts "Done seeding!" 
