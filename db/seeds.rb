f150c503cea3bd1445151c512617f8625645ce61
Pete Fowler <fowler.pete@gmail.com>
2022-09-27 10:14:49 -0600

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,30 @@
+require 'faker'
+
 puts "Seeding ..."
 
-10.times do
+11.times do
   Earthling.create(
     first_name: Faker::Name.first_name, 
     last_name: Faker::Name.last_name, 
-    job: Faker::job
+    job: Faker::Job.title 
   )
 
   Alien.create(
-    name: Faker::Name.first,
-    earth_disguise_name: Faker::Name.first,
+    name: Faker::Name.first_name,
+    earth_disguise_name: Faker::Name.first_name, 
     home_planet: Faker::Space.planet,
     light_years_to_home_planet: Faker::Space.distance_measurement
   )
 
-  Visitation.create(
-    date: Faker::Date.between(from: 2020-01-01 to: 2022-09-27), 
-    alien_id: Faker::Number.unique.non_zero_digit, 
-    earthling_id: Faker::Number.unique.non_zero_digit
-  )
+  
 end
+
+9.times do 
+  Visitation.create(
+    date: Faker::Date.between(from: '2020-01-01', to: '2022-09-27'), 
+    alien_id: Faker::Number.within(range: 1..10), 
+    earthling_id: Faker::Number.within(range: 1..10)  
+  )
+end 
 
 puts "Done seeding!" 
