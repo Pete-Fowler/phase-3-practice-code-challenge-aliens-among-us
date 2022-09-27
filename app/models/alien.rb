634c124539dffc108c0b775ef2ecc4b288afc09a
Pete Fowler <fowler.pete@gmail.com>
2022-09-27 10:53:59 -0600

    Add 2 alien methods

--- app/models/alien.rb
+++ app/models/alien.rb
@@ -2,5 +2,13 @@ class Alien < ActiveRecord::Base
   has_many :visitations 
   has_many :earthlings, through: :visitations
 
-  
+  def visitations 
+    Visitation.where(alien_id: self.id) 
+  end 
+
+  def earthlings
+    self.visitations.map do |v| 
+      Earthling.where(id: v.earthling_id)
+    end
+  end
 end
