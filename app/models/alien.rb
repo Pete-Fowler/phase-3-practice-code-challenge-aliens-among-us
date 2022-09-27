218d8539e21f6a85ba020cdab39653b8ae823d1f
Pete Fowler <fowler.pete@gmail.com>
2022-09-27 11:33:11 -0600

    Add most frequent visitor

--- app/models/alien.rb
+++ app/models/alien.rb
@@ -1,3 +1,5 @@
+require 'date' 
+
 class Alien < ActiveRecord::Base
   has_many :visitations 
   has_many :earthlings, through: :visitations
@@ -10,5 +12,19 @@ class Alien < ActiveRecord::Base
     self.visitations.map do |v| 
       Earthling.where(id: v.earthling_id)
     end
+  end
+
+  def visit(earthling)
+    d = Date.today 
+    Visitation.create(date: d, alien_id: self.id, earthling_id: earthling.id)
+  end
+
+  def total_light_years_traveled
+    self.light_years_to_home_planet * self.visitations.count * 2
+  end
+
+  def self.most_frequent_visitor
+    a = Visitation.maximum(:alien_id)
+    Alien.where(id: a) 
   end
 end
